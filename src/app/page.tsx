77c178d91e3fddd6629058df68892312ad3dc015
CameloDev <freefireops@outlook.com>
2025-06-16 17:33:22 -0400

    Adding eye icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,6 @@ export default function Login() {
               className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
             >
               {showPassword ? (
-                // eye-off icon (SVG)
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
