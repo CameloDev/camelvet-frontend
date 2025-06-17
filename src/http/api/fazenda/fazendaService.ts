c72ff168be6c3cd52aa15016341f8b89ad33b4c7
CameloDev <freefireops@outlook.com>
2025-06-17 18:14:49 -0400

    Added get farm by ID

--- src/http/api/fazenda/fazendaService.ts
+++ src/http/api/fazenda/fazendaService.ts
@@ -4,3 +4,12 @@ export const getFazendasDoUsuario = async () => {
   const response = await api.get("/api/fazenda/dashboard/me");
   return response.data;
 };
+export const getFazendaById = async (fazenda_id: string) => {
+  try {
+    const response = await api.get(`/api/fazenda/dashboard/${fazenda_id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao buscar fazenda com ID ${fazenda_id}:`, error);
+    throw error;
+  }
+};
